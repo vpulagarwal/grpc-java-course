da5388db3c49e25be344ed6af962eb7e27a4e2c1
Vipul Agarwal <36162277+vpulagarwal@users.noreply.github.com>
2020-03-24 18:04:57 +0530

    Reflection API, evans cli troubleshooting for windows

---
+++ README.md
@@ -0,0 +1,17 @@
+# gRPC Java
+
+
+## Troubleshooting
+### Evans CLI
+
+- Download : https://github.com/ktr0731/evans/releases - evans_windows_386.tar.gz
+
+- Run Evans command in `cmd`
+    - `evans -r -p 50051`
+    -  `show package`
+    - `show service`
+    - `show message`
+    - `package <packageName>`
+    - `service <serviceName>`
+    - `call <MethodName : eg. SumService>`
+    
--- src/main/java/com/github/vipul/grpc/calculator/server/CalculatorServer.java
+++ src/main/java/com/github/vipul/grpc/calculator/server/CalculatorServer.java
@@ -2,6 +2,7 @@ package com.github.vipul.grpc.calculator.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ public class CalculatorServer {
 
         Server server = ServerBuilder.forPort(50051)
                 .addService(new CalculatorServiceImpl())
+                .addService(ProtoReflectionService.newInstance()) // For reflection
                 .build();
         server.start();
 
